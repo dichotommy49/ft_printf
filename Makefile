5ddf24adf71cd7aee6dac7236e9f54fee8dba88e
dichotommy49 <56877004+dichotommy49@users.noreply.github.com>
2019-12-01 15:10:30 +0100

    added Makefile

---
+++ Makefile
@@ -0,0 +1,43 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tmelvin <marvin@42.fr>                     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/10/23 14:36:51 by tmelvin           #+#    #+#              #
+#    Updated: 2019/12/01 15:07:44 by tmelvin          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME := libftprintf.a
+SRCDIR := ./
+LIBDIR := libft
+SRC := ft_printf convert_alpha convert_numeric buffer get_info handle
+CC := gcc
+CFLAGS := -Wall -Wextra -Werror
+OPTIONS := -c
+CFILES := $(patsubst %, $(SRCDIR)%.c, $(SRC))
+OBJECTS := $(patsubst %, $(SRCDIR)%.o, $(SRC))
+
+all : $(NAME)
+
+%.o : %.c
+	$(CC) $(CFLAGS) $(OPTIONS) -o $@ $<
+$(NAME) : $(OBJECTS)
+	$(MAKE) -C $(LIBDIR) all
+	cp libft/libft.a $(NAME)
+	ar rc $(NAME) $(OBJECTS)
+	ranlib $(NAME)
+clean :
+	rm -f $(OBJECTS) $(BONUS_OBJECTS)
+	$(MAKE) -C $(LIBDIR) clean
+fclean : clean
+	rm -f $(NAME)
+	rm -f $(LIBDIR)/libft.a
+re : fclean all
+
+bonus : all 
+
+.PHONY : all clean fclean re bonus
+
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tmelvin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 16:09:34 by tmelvin           #+#    #+#             */
-/*   Updated: 2019/12/01 13:58:05 by tmelvin          ###   ########.fr       */
+/*   Updated: 2019/12/01 14:50:39 by tmelvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,17 @@
 
 int		main(void)
 {
-	char	s[] = "Just your average everyday string";
-	char	c = 'A';
-	int		d = 2147483647;
-	unsigned int u = 4147438648;
-	int x = 255;
-	int X = 0;
-	int	*p = &x;
+//	char	s[] = "Just your average everyday string";
+//	char	c = 'A';
+	int		d = 42;
+//	unsigned int u = 4147438648;
+//	int x = 255;
+//	int X = 0;
+//	int	*p = &x;
+//
+//	ft_printf("This is a string test: %s\nAnd this is a char test: %c\nNow we have an int test: %i\nThis is an unsigned int: %u\nHere is a lowercase hexadecimal conversion: %#x\nAnd an uppercase hex conversion: %#X\nFinally, a pointer conversion: %p\n", s, c, d, u, x, X, p);
+//	printf("This is the real function's pointer conversion: %p\n", p);
 
-	ft_printf("This is a string test: %s\nAnd this is a char test: %c\nNow we have an int test: %i\nThis is an unsigned int: %u\nHere is a lowercase hexadecimal conversion: %#x\nAnd an uppercase hex conversion: %#X\nFinally, a pointer conversion: %p\n", s, c, d, u, x, X, p);
-	printf("This is the real function's pointer conversion: %p\n", p);
-
+	ft_printf("This is a test of min width: |%5d|\n", d);
 	return (0);
 }
